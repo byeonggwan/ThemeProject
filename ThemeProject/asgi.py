27b5628665729b5f315697c54bc5b2ace83c6af1
byeonggwan <choosymail@gmail.com>
2020-07-07 13:57:37 +0900

    initial commit

---
+++ ThemeProject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ThemeProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ThemeProject.settings')
+
+application = get_asgi_application()
