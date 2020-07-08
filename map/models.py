e52a3855bc7dd895bb77a90f83ce5ac02cb1fa8f
byeonggwan <choosymail@gmail.com>
2020-07-08 13:24:45 +0900

    Implement CRUD

--- map/models.py
+++ map/models.py
@@ -10,32 +10,32 @@ class Big(models.Model):
 class Middle(models.Model):
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    big_theme = models.ForeignKey(Big, on_delete=models.CASCADE)
+    big_theme = models.ForeignKey(Big, related_name='middles',on_delete=models.CASCADE)
 
 
 class Stock(models.Model):
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    middle_theme = models.ManyToManyField(Middle, blank=True)
+    middle_theme = models.ManyToManyField(Middle, related_name='stock', blank=True)
 
 
 class BigComments(models.Model):
     title = models.CharField(max_length=100)
     body = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    located_at = models.ForeignKey(Big, on_delete=models.CASCADE)
+    located_at = models.ForeignKey(Big, related_name='comments', on_delete=models.CASCADE)
 
 class MiddleComments(models.Model):
     title = models.CharField(max_length=100)
     body = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    located_at = models.ForeignKey(Middle, on_delete=models.CASCADE)
+    located_at = models.ForeignKey(Middle, related_name='comments', on_delete=models.CASCADE)
 
 class StockComments(models.Model):
     title = models.CharField(max_length=100)
     body = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    located_at = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    located_at = models.ForeignKey(Stock, related_name='comments', on_delete=models.CASCADE)
 
 
 
--- map/serializers.py
+++ map/serializers.py
@@ -1,33 +1,12 @@
 from rest_framework import serializers
 from .models import Big, Middle, Stock, BigComments, MiddleComments, StockComments
 
-class BigSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(required=True, max_length=100)
-    
-    class Meta:
-        model = Big
-        fields = '__all__'
-
-class MiddleSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(required=True, max_length=100)
-
-    class Meta:
-        model = Middle
-        fields = '__all__'
-
-
-class StockSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(required=True, max_length=100)
-    
-    class Meta:
-        model = Stock
-        fields = '__all__'
 
 class BigCommentsSerializer(serializers.ModelSerializer):
     title = serializers.CharField(required=True, max_length=100)
     body = serializers.CharField(required=True, max_length=200)
-    located_at = serializers.CharField(required=True, max_length=100)
-
+    located_at = serializers.SlugRelatedField(slug_field='title', queryset=Big.objects.all())
+    
     class Meta:
         model = BigComments
         fields = '__all__'
@@ -35,7 +14,7 @@ class BigCommentsSerializer(serializers.ModelSerializer):
 class MiddleCommentsSerializer(serializers.ModelSerializer):
     title = serializers.CharField(required=True, max_length=100)
     body = serializers.CharField(required=True, max_length=200)
-    located_at = serializers.CharField(required=True, max_length=100)
+    located_at = serializers.SlugRelatedField(slug_field='title', queryset=Middle.objects.all())
 
     class Meta:
         model = MiddleComments
@@ -44,8 +23,37 @@ class MiddleCommentsSerializer(serializers.ModelSerializer):
 class StockCommentsSerializer(serializers.ModelSerializer):
     title = serializers.CharField(required=True, max_length=100)
     body = serializers.CharField(required=True, max_length=200)
-    located_at = serializers.CharField(required=True, max_length=100)
+    located_at = serializers.SlugRelatedField(slug_field='title', queryset=Stock.objects.all())
 
     class Meta:
         model = StockComments
         fields = '__all__'
+
+class StockSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(required=True, max_length=100)
+    middle_theme = serializers.SlugRelatedField(many=True, slug_field='title', queryset=Middle.objects.all())
+    comments = StockCommentsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Stock
+        fields = ('id', 'title', 'middle_theme', 'comments')
+
+class MiddleSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(required=True, max_length=100)
+    big_theme = serializers.SlugRelatedField(slug_field='title', queryset=Big.objects.all())
+    stock = StockSerializer(many=True, read_only=True)
+    comments = MiddleCommentsSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Middle
+        fields = ('id', 'title', 'big_theme', 'stock', 'comments')
+
+class BigSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(required=True, max_length=100)
+    middles = MiddleSerializer(many=True, read_only=True)
+    comments = BigCommentsSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Big
+        fields = '__all__'
+
